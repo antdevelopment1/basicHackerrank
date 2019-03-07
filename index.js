1ae0e02ad0684207d11e17282fadcd6918c9c623
April Copes <antdevelopment1@gmail.com>
2019-03-06 22:44:08 -0500

    super easy hacker rank problem

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function modifyArray(nums) {
     return newArr; 
 }
 console.log(modifyArray([1,2,3,4]))
+
+
+let obj = [
+    {'x': 1, 'y':1},
+    {'x': 1, 'y':2}
+]
+function getCount(objects) {
+    let count = 0;
+    for (let key of objects) {
+        if (key['x'] === key['y']) {
+            count += 1;
+        }
+    }
+    return count;
+    
+}
+console.log(getCount(obj))
